d9509ffd1cd5f785967e5116e606250b2c31ffd4
alma1902 <almakurbegovic_rm@yahoo.com>
2019-11-22 02:30:11 +0100

    Getters and setters

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,15 @@
+
+package javaapplication7;
+
+public class JavaApplication7 {
+
+     public static void main(String[] args) {
+     Person person = new Person();
+		person.setName("Uvuvwevwevwe Onyetenyevwe Ugwemubwem");
+		person.setLastName("Ossas");
+		person.setAge(28);
+
+		System.out.println("Full Name and age: " + person.getFullNameAndAge());
+     }
+     
+}
---
+++ src/javaapplication7/Person.java
@@ -0,0 +1,38 @@
+
+package javaapplication7;
+
+public class Person {
+
+	private String name;
+	private String lastName;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullNameAndAge() {
+		return name + " " + lastName + ", " + age;
+	}
+
+}
